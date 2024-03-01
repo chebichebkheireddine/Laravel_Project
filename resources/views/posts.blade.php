f7b76c530fde1595320a4df992180b5cd1424fc7
Chebicheb Kheireddine <76478942+chebichebkheireddine@users.noreply.github.com>
2024-03-01 11:31:09 +0100

    Store Blog Posts as HTML Files

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -12,7 +12,7 @@
 <body>
     <article>
         <h1>
-            <a href="/post">My first post</a>
+            <a href="/post/my-first-post">My first post</a>
         </h1>
         <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, veniam necessitatibus hic ut nemo facere officia eligendi facilis nihil in repellendus! Eum officia cumque ullam veritatis corrupti sit id dolor.
@@ -21,7 +21,7 @@
     </article>
     <article>
         <h1>
-            <a href="/post">My Secend post</a>
+            <a href="/post/my-secend-post">My Secend post</a>
         </h1>
         <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, veniam necessitatibus hic ut nemo facere officia eligendi facilis nihil in repellendus! Eum officia cumque ullam veritatis corrupti sit id dolor.
--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('posts');
 });
-Route::get('/post/{post}', function () {
-    $path=__DIR__."/../resources/posts/my-first-post.html";
+Route::get('/post/{post}', function ($solg) {
+    $path=__DIR__."/../resources/posts/{$solg}.html";
+    // protocol to path  if exsist
+if (!file_exists($path)) {
+    # Do  this
+    // dd("file is not here");
+    #this file is not here
+    abort(404);
+    // return redirect("/");
+}
+
     $post= file_get_contents($path);
     return view ("post",[
         "var"=>$post
