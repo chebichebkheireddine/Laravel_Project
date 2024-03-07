af19c7500ccf7929fb32361af675dc72266b0eff
Chebicheb Kheireddine <76478942+chebichebkheireddine@users.noreply.github.com>
2024-03-07 20:42:45 +0100

    Blade code

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,22 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel-8</title>
-    <link rel="stylesheet" href="/app.css">
-    <script src="/app.js"></script>
-</head>
-<body>
-    <article>
-        <!-- This call from Route in Web.php -->
-        <h1><?= $posts->title; ?></h1>
-        <p>
-        <?= $posts->body; ?>
-        </p>
-        <a href="/">Go back</a>
 
-    </article>
-        
-</body>
-</html>
+<x-layout>
+    <x-slot name="slot">
+        <article>
+            <!-- This call from Route in Web.php -->
+            <h1>{{ $posts->title }} </h1>
+            <p>
+                {{ $posts->body }}
+            </p>
+            <a href="/">Go back</a>
+
+        </article>
+    </x-slot>
+</x-layout>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,27 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel-8</title>
-    <link rel="stylesheet" href="/app.css">
-    <script src="/app.js"></script>
-</head>
-
-<body>
-    <?php foreach ($posts as $post) : ?>
+<x-layout>
+<x-slot name="slot">
+    @foreach ($posts as $post)
+        {{-- @dd($post) --}}
         <article>
 
             <h1>
-                <a href="/post/<?= $post->slug; ?>">
-                    <?= $post->title;  ?>
+                <a href="/post/{{ $post->slug }} ">
+                    {{ $post->title }}
+                    
                 </a>
             </h1>
-            <p><?= $post->subPar; ?></p>
+            <p> {{ $post->subPar }} </p>
 
         </article>
-    <?php endforeach; ?>
-</body>
+    @endforeach
 
-</html>
+</x-slot>
+
+</x-layout>
+
