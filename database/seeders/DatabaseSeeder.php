36791bc44470462671341606ef7a4931fd1ff6e0
Chebicheb Kheireddine <76478942+chebichebkheireddine@users.noreply.github.com>
2024-03-16 01:02:01 +0100

    DataFile

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,39 +17,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $user=User::factory(12)->create();
-        Post::factory(2)->create();
-        // User::truncate();
-        // Category::truncate();
-        // Post::truncate();
-        // $user = User::factory()->create();
-        // $work=Category::create([
-        //     "name"=>'Codeing',
-        //       "slug"=>'test'
-        // ]); 
-        // $fun=Category::create([
-        //     "name"=>'play',
-        //       "slug"=>'Games'
-        // ]); 
-        // Post::create(
-        //     [
-        //         "user_id"=>$user->id,
-        //         "category_id"=>$work->id,
-        //         "slug"=>"Coding",
-        //         "title"=>"My Work post",
-        //         "excerpt"=>"Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-        //         "body"=>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi ea, nam corporis quasi inventore, porro eveniet nesciunt vel earum at expedita vitae totam? Impedit dignissimos, fugit dolorem in voluptatem quo."  
-        //     ]
-        // );
-        // Post::create(
-        //     [
-        //         "user_id"=>$user->id,
-        //         "category_id"=>$fun->id,
-        //         "title"=>"My Fun post",
-        //         "slug"=>"use",
-        //         "excerpt"=>"Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-        //         "body"=>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi ea, nam corporis quasi inventore, porro eveniet nesciunt vel earum at expedita vitae totam? Impedit dignissimos, fugit dolorem in voluptatem quo."  
-        //     ]
-        // );
+        $user=User::factory()->create();
+        Post::factory(9)->create([
+            "user_id"=>$user->id
+        ]);
     }
 }
